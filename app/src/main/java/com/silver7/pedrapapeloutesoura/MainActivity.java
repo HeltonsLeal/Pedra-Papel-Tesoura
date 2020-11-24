ea053de7e0f10af6c0e3d449f5d7ef3ce50cf741
HeltonsLeal <hsleal1984@gmail.com>
2020-11-24 17:57:23 -0300

    projeto01

--- app/src/main/java/com/silver7/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/silver7/pedrapapeloutesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.silver7.pedrapapeloutesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void pedraSelecionada(View view){
+        this.opcaoSelecionada("Pedra");
+    }
+
+    public void papelSelecionada(View view){
+        this.opcaoSelecionada("Papel");
+    }
+
+    public void tesouraSelecionada(View view){
+        this.opcaoSelecionada("Tesoura");
+    }
+    public void opcaoSelecionada(String opcaoSelecionada){
+
+        System.out.println("Iten selecionado: " + opcaoSelecionada );
+
+    }
+
+
 }
