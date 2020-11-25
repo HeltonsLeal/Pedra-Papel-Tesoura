c1ae5506723735a13ebc48e54154918510a5048e
HeltonsLeal <hsleal@gmail.com>
2020-11-24 23:39:54 -0300

    pronto

--- app/src/main/java/com/silver7/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/silver7/pedrapapeloutesoura/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,19 +18,83 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void pedraSelecionada(View view){
-        this.opcaoSelecionada("Pedra");
+
+        this.opcaoSelecionada("pedra");
     }
 
     public void papelSelecionada(View view){
-        this.opcaoSelecionada("Papel");
+
+
+        this.opcaoSelecionada("papel");
     }
+
 
     public void tesouraSelecionada(View view){
-        this.opcaoSelecionada("Tesoura");
+        this.opcaoSelecionada("tesoura");
     }
+
     public void opcaoSelecionada(String opcaoSelecionada){
 
-        System.out.println("Iten selecionado: " + opcaoSelecionada );
+        ImageView imagemResultado = findViewById(R.id.imagemResultado);
+        TextView textoResultado = findViewById(R.id.textResultado);
+        int app = new Random().nextInt(3);
+        String [] opcoes = {"pedra","papel","tesoura"};
+        String opcaoApp = opcoes [app];
+
+        switch (opcaoApp){
+            case "pedra" :
+                imagemResultado.setImageResource(R.drawable.pedra);
+                break;
+
+            case "papel" :
+                imagemResultado.setImageResource(R.drawable.papel);
+                break;
+
+            case "tesoura" :
+                imagemResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
+
+        if (
+                (opcaoApp == "pedra" && opcaoSelecionada == "tesoura") ||
+                (opcaoApp == "papel" && opcaoSelecionada == "pedra") ||
+                (opcaoApp == "tesoura" && opcaoSelecionada == "papel")
+        ){
+            textoResultado.setText("Você Perdeu");
+        }else if(
+                (opcaoSelecionada == "pedra" && opcaoApp == "tesoura") ||
+                (opcaoSelecionada == "papel" && opcaoApp == "pedra") ||
+                (opcaoSelecionada == "tesoura" && opcaoApp == "papel")
+        ){
+            textoResultado.setText("Você Ganhou");
+
+        }else{
+            textoResultado.setText("Deu Empate");
+
+        }
+
+        System.out.println("Quero Resultado "+opcaoApp);
+        System.out.println("Quero Resultado "+opcaoSelecionada);
+        System.out.println("Quero Resultado "+textoResultado);
+/*
+        if (opcaoSelecionada == opcaoApp) {
+            texto.setText("Deu EMPATE");
+        } else if (opcaoSelecionada == "pedra" && opcaoApp == "tesoura") {
+            texto.setText("Você GANHOU");
+        } else if (opcaoSelecionada == "papel" && opcaoApp == "pedra") {
+            texto.setText("Você GANHOU");
+        } else if (opcaoSelecionada == "tesoura" && opcaoApp == "papel") {
+            texto.setText("Você GANHOU");
+        } else if (opcaoSelecionada == "pedra" && opcaoApp == "papel") {
+            texto.setText("Você PERDEU");
+        } else if (opcaoSelecionada == "papel" && opcaoApp == "tesoura") {
+            texto.setText("Você PERDEU");
+        } else {
+            texto.setText("Você PERDEU");
+        }
+*/
+
+
 
     }
 
